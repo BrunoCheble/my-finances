a77cac6968f9c431169a5fd906c978c9cb642ea9
Bruno Cheble <contato.cheble@gmail.com>
2025-11-09 23:26:23 +0000

    fix: Corrigir a tabela de migração para adicionar índices nas tabelas financeiras

--- database/migrations/2025_11_09_094918_add_indexes_to_financial_tables.php
+++ database/migrations/2025_11_09_094918_add_indexes_to_financial_tables.php
@@ -4,25 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::table('financial_tables', function (Blueprint $table) {
-            //
+        Schema::table('financial_balances', function (Blueprint $table) {
+            $table->index('start_date', 'idx_financial_balances_start_date');
+            $table->index('end_date', 'idx_financial_balances_end_date');
+        });
+
+        Schema::table('financial_movements', function (Blueprint $table) {
+            $table->index('date', 'idx_financial_movements_date');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('financial_tables', function (Blueprint $table) {
-            //
+        Schema::table('financial_balances', function (Blueprint $table) {
+            $table->dropIndex('idx_financial_balances_start_date');
+            $table->dropIndex('idx_financial_balances_end_date');
+        });
+
+        Schema::table('financial_movements', function (Blueprint $table) {
+            $table->dropIndex('idx_financial_movements_date');
         });
     }
 };
